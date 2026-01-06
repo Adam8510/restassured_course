c8d53fe76d437191acc804984093e8d0c9f79c5f
Adam <adammoskal7@gmail.com>
2026-01-06 15:22:18 +0100

    deserialization with Task5.7 finished

--- src/test/java/restassured/test/tasks/UserCreationTests.java
+++ src/test/java/restassured/test/tasks/UserCreationTests.java
@@ -1,29 +1,31 @@
-package http.methods;
+package restassured.test.tasks;
 
 import org.testng.annotations.Test;
+import pl.javastart.main.pojo.User;
 
 import static io.restassured.RestAssured.given;
 
 public class UserCreationTests {
     @Test
     public void givenCorrectUserDataWhenCreateUserThenUserIsCreatedTest() {
+
+        User user = new User();
+        user.setId(445);
+        user.setUsername("firstuser");
+        user.setFirstName("Krzysztof");
+        user.setLastName("Kowalski");
+        user.setEmail("krzysztof@test.com");
+        user.setPassword("password");
+        user.setPhone("+123456789");
+        user.setUserStatus(123);
         given().log().all()
                 .contentType("application/json")
-                .body("{\n" +
-                        "  \"id\": 445,\n" +
-                        "  \"username\": \"firstuser\",\n" +
-                        "  \"firstName\": \"Krzysztof\",\n" +
-                        "  \"lastName\": \"Kowalski\",\n" +
-                        "  \"email\": \"krzysztof@test.com\",\n" +
-                        "  \"password\": \"password\",\n" +
-                        "  \"phone\": \"+123456789\",\n" +
-                        "  \"userStatus\": 123\n" +
-                        "}")
+                .body(user)
                 .when().post("https://swaggerpetstore.przyklady.javastart.pl/v2/user")
                 .then().log().all().statusCode(200);
 
         given().log().all()
-                .pathParam("username", "firstuser")
+                .pathParam("username", user.getUsername())
                 .when().get("https://swaggerpetstore.przyklady.javastart.pl/v2/user/{username}")
                 .then().log().all().statusCode(200);
     }
